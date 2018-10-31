35927aedcf960e3c1470b824061dcefaf19bd67b
Roy Xing <00royxing@gmail.com>
2018-10-31 15:33:08 -0700

    Add files via upload
    TODO: Change the way you're tuning the k, technically there's no "correct" way to approach it as there was with tuning the initial conditions

--- raibertPController.m
+++ raibertPController.m
@@ -6,13 +6,19 @@ function [xf, theta] = raibertPController(q, s, t)
 
     % This is the feedback gain for ft displacement from neutral point
     % measured in m/(m/s), which is just s
-    k = -.12531;
+    k = -0.0595; %got to around the fraction -43/726 and then called it quits
+    
+    % NOTE: for tuning the gain there's not a general "rule" for
+    % approaching a number, try random numbers and then tune from the best
+    % there
+    k = -0.0475432;
     xf = 0;
     theta = 0;
     
     if q(end, 6) == 0 % If the model is in flight phase
         
-        theta = s.theta + k * (q(end, 2) - s.d_fwrd_vel)
+        %theta = s.theta + k * (q(end, 2) - s.d_fwrd_vel);
+        theta = q(end, 7) + k * (q(end, 2) - s.d_fwrd_vel);
         
         xf = cos(theta) / s.d0;
         
